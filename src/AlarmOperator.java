f31c13198e85f075dbda3e2bcfdb93c48920d662
hodeiez <hodeinaiz@hotmail.com>
2024-09-14 11:34:39 +0200

    add more functions to operator

--- src/AlarmOperator.java
+++ src/AlarmOperator.java
@@ -10,20 +10,36 @@ public class AlarmOperator implements AlarmChecker{
           this.houses.add(house);
       }
     }
-    public void activateHouseAllSensors(String selectedHouseId) {
-        this.houses.stream().filter(house -> house.getHouseId().equals(selectedHouseId)).findFirst().get().getRooms().forEach(room -> room.getDoors().forEach(door -> door.setActivated(true)));
-    }
-    public void deActivateHouseAllSensors(String selectedHouseId) {
-        this.houses.stream().filter(house -> house.getHouseId().equals(selectedHouseId)).findFirst().get().getRooms().forEach(room -> room.getDoors().forEach(door -> door.setActivated(false)));
+    private House getHouse(String selectedHouseId) {
+        return this.houses.stream().filter(house -> house.getHouseId().equals(selectedHouseId)).findFirst().get();
     }
 
+    public void activateHouseAllSensors(String selectedHouseId) {
+        this.getHouse(selectedHouseId).getRooms().forEach(room -> room.getDoors().forEach(door -> door.setActivated(true)));
+    }
+    public void deActivateHouseAllSensors(String selectedHouseId) {
+       this.getHouse(selectedHouseId).getRooms().forEach(room -> room.getDoors().forEach(door -> door.setActivated(false)));
+    }
+    public void activateAllSensorsInRoom(String selecteHouseId, String selectedRoom) {
+        this.operateAllSensorsInRoom(selecteHouseId,selectedRoom,true);
+    }
+    public void deActivateAllSensorsInRoom(String selecteHouseId, String selectedRoom) {
+        this.operateAllSensorsInRoom(selecteHouseId,selectedRoom,false);
+    }
+   private void operateAllSensorsInRoom(String selecteHouseId, String selectedRoom, boolean activate) {
+        this.getRoomAtHouse(selecteHouseId, selectedRoom).getDoors().forEach(door -> door.setActivated(activate));
+    }
+
+    public Room getRoomAtHouse(String selectedHouseId, String selectedRoom) {
+       return this.getHouse(selectedHouseId).getRooms().stream().filter(room -> room.getRoomName().equals(selectedRoom)).findFirst().get();
+    }
     @Override
     public void checkAlarm() {
         this.houses.forEach(house -> {
                 house.getRooms().forEach(room ->{
             room.getDoors().forEach(door -> {
                 if (door.isDetected()) {
-                    System.out.println("Door open at room -> " + room.getRoomName());
+                    System.out.println("Door open at "+ house.getHouseId() + "house in room -> " + room.getRoomName());
                 } else {
                     System.out.println("check Alarm");
                 }
