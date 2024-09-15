735270b2047cf8b525b97a0d453e06a5bb4a2219
hodeiez <hodeinaiz@hotmail.com>
2024-09-15 19:59:54 +0200

    add more functions

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -1,4 +1,9 @@
 package gui;
 
 public class Menu {
+
+    public void showMainMenu() {
+        System.out.println("\n1.-Get configuration info\n2.-Deactivate everything\n3.-Deactivate a room\n4.-Run simulation");
+    }
+
 }
